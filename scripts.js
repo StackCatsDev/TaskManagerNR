763b2c3274f8ce7c93281d1bf35a030c516c7413
Jelofen1962 <amirrezataati1962@gmail.com>
2024-09-12 02:27:00 +0330

    changes 11

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             localStorage.setItem('notes', JSON.stringify(savedNotes));
             noteTitle.value = '';
             noteInput.value = '';
-            setTimeout(window.location.replace("./TaskManagerNR/"),1000)
+            setTimeout(history.back(), 1000);
         }
     };
 
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const savedNotes = JSON.parse(localStorage.getItem('notes')) || [];
         savedNotes.splice(index, 1);
         localStorage.setItem('notes', JSON.stringify(savedNotes));
-        setTimeout(window.location.replace("./"),1000)
+        setTimeout(history.back(), 1000);
     };
 });
 
